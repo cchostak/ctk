a567ce7d5922e62243bcfbbb643e7c68b9181691
Christian Chostak <c.chostak@gmail.com>
2018-05-21 16:01:26 +0100

    remoção do ficheiro access, lógica implementada no handler

--- handler.lua
+++ handler.lua
@@ -2,30 +2,52 @@ local singletons = require "kong.singletons"
 local BasePlugin = require "kong.plugins.base_plugin"
 local responses = require "kong.tools.responses"
 local constants = require "kong.constants"
-local access = require "kong.plugins.ctk.access"
 
 local ipairs         = ipairs
 local string_format  = string.format
 local ngx_re_gmatch  = ngx.re.gmatch
 local ngx_set_header = ngx.req.set_header
 local get_method     = ngx.req.get_method
+local multipart = require "multipart"
+local cjson = require "cjson"
 
-CTK.PRIORITY = 1200
-CTK.VERSION = "0.0.1"
+local _M = {}
+
+local table_insert = table.insert
+local req_set_uri_args = ngx.req.set_uri_args
+local req_get_uri_args = ngx.req.get_uri_args
+local req_set_header = ngx.req.set_header
+local req_get_headers = ngx.req.get_headers
+local req_read_body = ngx.req.read_body
+local req_set_body_data = ngx.req.set_body_data
+local req_get_body_data = ngx.req.get_body_data
+local req_clear_header = ngx.req.clear_header
+local req_set_method = ngx.req.set_method
+local encode_args = ngx.encode_args
+local ngx_decode_args = ngx.decode_args
+local type = type
+local string_find = string.find
+local pcall = pcall
+local ngx_re_gmatch  = ngx.re.gmatch
+
+local CONTENT_LENGTH = "content-length"
+local CONTENT_TYPE = "content-type"
+local HOST = "host"
 
 local CtkHandler = BasePlugin:extend()
 
 function CtkHandler:new()
   CtkHandler.super.new(self, "ctk")
+  
 end
 
 function CtkHandler:access(conf)
-  access.execute(conf)
-  -- print(config.environment) 
-  -- print(config.server.host)
-  -- print(config.server.port)
-  -- print(config.key_names)
-  -- print(config.hide_credentials)
+  CtkHandler.super.access(self)
+
+  local authorization_header = req_get_headers()["authorization"],
+  local jwt_hash = ngx_re_gmatch(authorization_header, "\\s*[Bb]earer\\s+(.+)"),
+  ngx.req.set_uri(ngx.unescape_uri("\\" .. jwt_hash))
+
 end
 
 return CtkHandler
--- schema.lua
+++ schema.lua
@@ -1,60 +1,7 @@
-local utils = require "kong.tools.utils"
-local find = string.find
-
 return {
   no_consumer = true,
   fields = {
-    http_method = {type = "string", func = check_method},
     key_names = {type = "array", required = true, default = {"ctk"}},
-    remove = {
-      type = "table",
-      schema = {
-        fields = {
-          body = {type = "array", default = {}}, -- does not need colons
-          headers = {type = "array", default = {}}, -- does not need colons
-          querystring = {type = "array", default = {}} -- does not need colons
-        }
-      }
-    },
-    rename = {
-      type = "table",
-      schema = {
-        fields = {
-          body = {type = "array", default = {}},
-          headers = {type = "array", default = {}},
-          querystring = {type = "array", default = {}}
-        }
-      }
-    }
-    -- replace = {
-    --   type = "table",
-    --   schema = {
-    --     fields = {
-    --       body = {type = "array", default = {}, func = check_for_value},
-    --       headers = {type = "array", default = {}, func = check_for_value},
-    --       querystring = {type = "array", default = {}, func = check_for_value}
-    --     }
-    --   }
-    -- },
-    -- add = {
-    --   type = "table",
-    --   schema = {
-    --     fields = {
-    --       body = {type = "array", default = {}, func = check_for_value},
-    --       headers = {type = "array", default = {}, func = check_for_value},
-    --       querystring = {type = "array", default = {}, func = check_for_value}
-    --     }
-    --   }
-    -- },
-    -- append = {
-    --   type = "table",
-    --   schema = {
-    --     fields = {
-    --       body = {type = "array", default = {}, func = check_for_value},
-    --       headers = {type = "array", default = {}, func = check_for_value},
-    --       querystring = {type = "array", default = {}, func = check_for_value}
-    --     }
-    --   }
-    -- }
+    hide_credentials = {type = "boolean", default = false}
   }
 }
