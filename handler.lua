fa611877842aa0463947bb8ba72b19b2e228e8df
Christian Chostak <c.chostak@gmail.com>
2018-05-23 12:23:07 +0100

    adicionado libs de roteamento, alterado metodo de upstream

--- handler.lua
+++ handler.lua
@@ -9,27 +9,19 @@ local url = require "socket.url"
 local access = require "kong.plugins.ctk.access"
 
 local CtkHandler = BasePlugin:extend()
+ngx.log(ngx.WARN, "JUST EXTENDED THE BASE PLUGIN")
 
 CtkHandler.PRIORITY = 3505
 CtkHandler.VERSION = "0.1.0"
 
-file = io.open("/usr/local/kong/logs/ctk.lua", "a+")
-io.input(file)
-file:write("--- JUST EXTENDED THE BASE PLUGIN ---")
-
-
 function CtkHandler:new()
   CtkHandler.super.new(self, "ctk")
-  file = io.open("/usr/local/kong/logs/ctk.lua", "a+")
-  io.input(file)
-  file:write("--- INSTACIATED ITSELF ---")  
+  ngx.log(ngx.WARN, "INSTANCIATED ITSELF")
 end
 
 function CtkHandler:access(conf)
   CtkHandler.super.access(self)
-  -- file = io.open("/usr/local/kong/logs/ctk.lua", "a+")
-  -- io.input(file)
-  -- file:write("--- STARTED THE ACCESS PART ---")
+  ngx.log(ngx.WARN, "RUNNING THE ACCESS BLOCK")
   access.execute(conf)
 end
 
