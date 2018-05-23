e2455a57a8d168302e84ce4216c68ec202c72c49
Christian Chostak <c.chostak@gmail.com>
2018-05-23 11:56:38 +0100

    adicionado libs de roteamento, alterado metodo de upstream

--- handler.lua
+++ handler.lua
@@ -1,38 +1,67 @@
-local singletons = require "kong.singletons"
+-- local singletons = require "kong.singletons"
+-- local BasePlugin = require "kong.plugins.base_plugin"
+-- local responses = require "kong.tools.responses"
+-- local utils = require "kong.tools.utils"
+-- local constants = require "kong.constants"
+-- local multipart = require "multipart"
+-- local cjson = require "cjson"
+-- local url = require "socket.url"
+-- local access = require "kong.plugins.ctk.access"
+
+-- local CtkHandler = BasePlugin:extend()
+
+-- CtkHandler.PRIORITY = 3505
+-- CtkHandler.VERSION = "0.1.0"
+
+-- file = io.open("/usr/local/kong/logs/ctk.lua", "a+")
+-- io.input(file)
+-- file:write("--- JUST EXTENDED THE BASE PLUGIN ---")
+
+
+-- function CtkHandler:new()
+--   CtkHandler.super.new(self, "ctk")
+--   file = io.open("/usr/local/kong/logs/ctk.lua", "a+")
+--   io.input(file)
+--   file:write("--- INSTACIATED ITSELF ---")  
+-- end
+
+-- function CtkHandler:access(conf)
+--   CtkHandler.super.access(self)
+--   file = io.open("/usr/local/kong/logs/ctk.lua", "a+")
+--   io.input(file)
+--   file:write("--- STARTED THE ACCESS PART ---")
+--   access.execute(conf)
+-- end
+
+-- file:close()
+
+-- return CtkHandler
+
+
 local BasePlugin = require "kong.plugins.base_plugin"
-local responses = require "kong.tools.responses"
-local utils = require "kong.tools.utils"
-local constants = require "kong.constants"
-local multipart = require "multipart"
-local cjson = require "cjson"
-local url = require "socket.url"
 local access = require "kong.plugins.ctk.access"
 
-local CtkHandler = BasePlugin:extend()
-
-CtkHandler.PRIORITY = 3505
-CtkHandler.VERSION = "0.1.0"
-
+local ChostakHandler = BasePlugin:extend()
 file = io.open("/usr/local/kong/logs/ctk.lua", "a+")
 io.input(file)
 file:write("--- JUST EXTENDED THE BASE PLUGIN ---")
 
-
-function CtkHandler:new()
-  CtkHandler.super.new(self, "ctk")
+function ChostakHandler:new()
+  ChostakHandler.super.new(self, "basic-auth")
   file = io.open("/usr/local/kong/logs/ctk.lua", "a+")
   io.input(file)
   file:write("--- INSTACIATED ITSELF ---")  
 end
 
-function CtkHandler:access(conf)
-  CtkHandler.super.access(self)
+function ChostakHandler:access(conf)
+  ChostakHandler.super.access(self)
+  access.execute(conf)
   file = io.open("/usr/local/kong/logs/ctk.lua", "a+")
   io.input(file)
   file:write("--- STARTED THE ACCESS PART ---")
-  access.execute(conf)
 end
 
-file:close()
+ChostakHandler.PRIORITY = 1001
+ChostakHandler.VERSION = "0.1.0"
 
-return CtkHandler
+return ChostakHandler
