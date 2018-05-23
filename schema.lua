19c5b696e96ce78f1f6b53d5a3da4cf021092641
Christian Chostak <c.chostak@gmail.com>
2018-05-23 11:41:33 +0100

    adicionado libs de roteamento, alterado metodo de upstream

--- schema.lua
+++ schema.lua
@@ -7,7 +7,7 @@ return {
   fields = {
     key_names = {type = "array", required = true, default = {"ctk"}},
     method = { default = "POST", enum = { "POST", "HEAD", } },
-    run_on_preflight = {type = "boolean", default = true},
+    run_on_preflight = {type = "boolean", default = true, func = do_authentication},
     uri_param_names = {type = "array", default = {"jwt"}},
     content_type = { default = "application/json", enum = { "application/json" } }
   },
