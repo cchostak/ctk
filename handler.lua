7d29a12940a8a7343c754ad9875c7697b9a9a048
Christian Chostak <c.chostak@gmail.com>
2018-05-25 16:24:47 +0100

    Plugin funcionando

--- handler.lua
+++ handler.lua
@@ -19,35 +19,28 @@ local request = ngx.request
 local CtkHandler = BasePlugin:extend()
 CtkHandler.PRIORITY = 3505
 CtkHandler.VERSION = "0.1.0"
-ngx.log(ngx.WARN, "--- ctk --- JUST EXTENDED THE BASE PLUGIN")
 
 function CtkHandler:new()
   CtkHandler.super.new(self, "ctk")
-  ngx.log(ngx.WARN, "--- ctk --- INSTACIATED ITSELF")
 end
-
-
 
 function CtkHandler:access(conf)
         CtkHandler.super.access(self)
-        ngx.log(ngx.WARN, "--- ctk --- STARTED THE ACCESS PROCESS")
-
+        uriRetrieved = ngx.var.uri
+        host = ngx.var.host
         -- GET JWT FROM HEADER AND ASSIGN TO TOKEN VARIABLE
         token = ngx.req.get_headers()["Authorization"]
         -- CHECK WHETER THE JWT EXISTS OR NOT
         if token == nil then
                 ngx.log(ngx.CRIT, "--- FORBIDDEN ---")
+                ngx.log(ngx.CRIT, token)
                 return responses.send_HTTP_FORBIDDEN("You cannot consume this service")
         else
-                ngx.log(ngx.WARN, token)
+                ngx.log(ngx.CRIT, "--- TOKEN ---")
+                ngx.log(ngx.CRIT, token)
                 -- SET THE UPSTREAM URI TO ANOTHER SERVICE
-                ngx.var.upstream_uri = "http://localhost:8000/authenticate/" .. token
-                -- ASSIGN THE NEW UPSTREAM URI TO ANOTHER VARIABLE
-                url = ngx.var.upstream_uri
-                ngx.log(ngx.WARN, tostring(ngx.var.upstream_uri))
-                -- THE REDIRECT THAT I'M TRYING TO GET RID OFF
-                redirect = ngx.redirect(url, ngx.HTTP_TEMPORARY_REDIRECT)
-                -- THE IMPLEMENTATION OF SUBREQUEST TO LOCAL PROXY
+                ura = "http://192.168.50.172:3315/v1/usr/access/" .. token
+                -- THE HTTP REQUEST THAT TEST IF JWT IS VALID OR NOT
                 local data = ""
 
                 local function collect(chunk)
@@ -59,29 +52,24 @@ function CtkHandler:access(conf)
 
                 local ok, statusCode, headers, statusText = http.request {
                         method = "POST",
-                        url = url,
+                        url = ura,
                         sink = collect
                 }
 
-                ngx.log(ngx.CRIT, statusCode)
-                ngx.log(ngx.CRIT, ok)
-                ngx.log(ngx.CRIT, headers)
-                ngx.log(ngx.CRIT, statusText)
-                for i,v in pairs(headers) do
-                print("\t",i, v)
+                -- THE STATUS CODE RETRIEVED FROM THE SERVICE
+                if statusCode == 200 then
+                        ngx.log(ngx.CRIT, "### STATUS 200 OK ###")
+                        ngx.log(ngx.CRIT, uriRetrieved)
+                else
+                        ngx.log(ngx.CRIT, "### NÃO AUTORIZADO ###")
+                        return responses.send_HTTP_FORBIDDEN("You cannot consume this service")
                 end
-
-                print("data", data)
 
                         end
                 end
 
-     -- THE DOCUMENTATION PROVIDES THAT THE HEADER_FILTER IS ALWAYS EXECUTED AFTER ALL BYTES WERE RECEIVED FROM THE UPSTREAM
 function CtkHandler:header_filter(conf)
         CtkHandler.super.header_filter(self)
-        ngx.log(ngx.WARN, "--- INICIO DO HEADER FILTER ---")
-
 end
-
 
 return CtkHandler
